075df36d6e7b13a3c5223c4f0de08b07274c11ef
Tung Pham <tungpham@tungs-mbp.mynetworksettings.com>
2023-10-06 10:31:06 -0400

    assignment from lesson_1_2

---
+++ src/AddTodoForm.js
@@ -0,0 +1,11 @@
+const AddTodoForm = () => {
+  return (
+    <form>
+        <label htmlFor="todoTitle">Title</label>
+        <input id="todoTitle" />
+        <button>Add</button>
+    </form>
+  )
+};
+
+export default AddTodoForm;
--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React from "react";
-
-const todoList = [
-  { id: 1, title: "Buy grocery" },
-  { id: 2, title: "Cook dinner" },
-  { id: 3, title: "Wash dishes" },
-];
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
   return (
     <>
-      <h1>Todo List</h1>
-      <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <TodoList />
+      <AddTodoForm />
     </>
   );
 }
