940706a05227ddf8aafe143145cc8f8581478af4
Tung Pham <tungpham@tungs-mbp.mynetworksettings.com>
2023-11-17 09:38:26 -0500

    assignment for lesson_1_5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = useState(() => {
+    const savedTodoList = localStorage.getItem("savedTodoList");
+    return savedTodoList ? JSON.parse(savedTodoList) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+};
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
+
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
